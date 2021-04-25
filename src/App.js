85cacd36dcbd5bc4a01b2c49ab6f8cfb6830518c
V-Sugak <vicka.sugak2010@yandex.ru>
2021-04-25 08:43:02 +0300

    19-lesson

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React, { Profiler } from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
+import { Dialogs } from './components/Dialogs/Dialogs'
 
 const App = () => {
   return (
+    <BrowserRouter>
     <div className="app-wrapper">
-     <Header />
-     <Navbar/>
-     <Profile/>
+      <Header />
+      <Navbar />
+      {/*<Dialogs/>*/}
+      <div class="app-wrapper-content">
+        <Route path='/profile' component={Profile} />
+        <Route path='/dialogs' component={Dialogs} />
+        
+      </div>
     </div>
+    </BrowserRouter>
   );
 }
 
