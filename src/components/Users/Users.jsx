3a7145976bbe8a5d89ada97db5570a3299fee809
V-Sugak <vicka.sugak2010@yandex.ru>
2022-05-27 21:22:34 +0300

    Users API

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,59 +1,33 @@
 import React from 'react';
 import s from './Users.module.css'
+import axios from "axios";
+import icon from './../../assets/images/icon.png'
+
 
 export const Users = (props) => {
-    if (props.users.length === 0) {
-        props.setUsers([{
-            id: 1,
-            photo: 'https://media.vogue.ru/photos/5ef5995856a994c4560ced25/master/pass/%D0%B2%D0%B8%D0%BA%D0%B01.jpg',
-            isFollowed: false,
-            fullName: 'Tory K.',
-            status: 'Hello',
-            location: {
-                country: 'RB',
-                city: 'Minsk',
-            },
-        },
-            {
-                id: 2,
-                photo: 'https://media.vogue.ru/photos/5ef5995856a994c4560ced25/master/pass/%D0%B2%D0%B8%D0%BA%D0%B01.jpg',
-                isFollowed: false,
-                fullName: 'Ira T.',
-                status: 'Yo',
-                location: {
-                    country: 'Russia',
-                    city: 'Moscow',
-                },
-            },
-            {
-                id: 3,
-                photo: 'https://media.vogue.ru/photos/5ef5995856a994c4560ced25/master/pass/%D0%B2%D0%B8%D0%BA%D0%B01.jpg',
-                isFollowed: false,
-                fullName: 'Dima V.',
-                status: 'Yes',
-                location: {
-                    country: 'RB',
-                    city: 'Minsk',
-                },
-
-            }])
+    const setUsers = () => {
+        if (props.users.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(res => {
+                props.setUsers(res.data.items)
+            })
+        }
     }
+    debugger
+    console.log(props.users)
     return <div>
-        {props.users.map(user => {
-            return <div>
+        <button onClick={setUsers}>Set Users</button>
+
+        {props.users.map(u => {
+            return <div key={u.id}>
         <span>
          <div>
-             <img className={s.photo} src={user.photo}/>
+       <div><img src={u.photos.small !==null ? u.photos.small :  icon} className={s.photo}/></div>
          </div>
-             <button onClick={() => props.isFollowed(user.id)}>{user.isFollowed ? 'Follow' : 'Unfollow'}</button>
+            {/*  <button onClick={() => props.isFollowed(u.id)}>{u.followed ? 'Follow' : 'Unfollow'}</button>*/}
         </span>
                 <span>
-               <div>{user.fullName}</div>
-            <div>{user.status}</div>
-        </span>
-                <span>
-               <div>{user.location.country}</div>
-            <div>{user.location.city}</div>
+               <div>{u.name}</div>
+            <div>{u.status}</div>
         </span>
             </div>
         })}
